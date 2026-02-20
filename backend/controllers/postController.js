059f3d45855f47dc0d3db32f6e76e80797f39b7b
nazim <nachunazim16@gmail.com>
2026-02-20 21:41:49 +0300

    get all posts, like and unlike, add & delete comment, edit caption

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -63,3 +63,93 @@ export const deletePost = async (req, res) => {
     }
 }
 
+export const getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find({type: "post"}).sort({ createdAt: -1 }).populate("owner", "-password");
+        const reels = await Post.find({ type: "reel" }).sort({ createdAt: -1 }).populate("owner", "-password");
+
+        res.json({posts, reels});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const likeUnlikePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({ message: "Post not found" });
+
+        if (post.likes.includes(req.user._id)) {
+            const index = post.likes.indexOf(req.user._id);
+            post.likes.splice(index, 1);
+
+            await post.save();
+            res.json({ message: "Post unliked" });
+        } else {
+            post.likes.push(req.user._id);
+            await post.save();
+            res.json({ message: "Post liked" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const commentOnPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({ message: "Post not found" });
+
+        post.comments.push({
+            user: req.user._id,
+            name: req.user.name,
+            comment: req.body.comment,
+        });
+
+        await post.save();
+        res.json({ message: "Comment added" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const deleteComment = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({ message: "Post not found" });
+
+        if (!req.body.commentId) return res.status(404).json({ message: "Provide a comment id" });
+        
+        const commentIndex = post.comments.findIndex(
+            (item) => item._id.toString() === req.body.commentId.toString()
+        );
+        if (commentIndex === -1) return res.status(404).json({ message: "Comment not found" });
+
+        const comment = post.comments[commentIndex];
+        if (post.owner.toString() === req.user._id.toString() || comment.user.toString() === req.user._id.toString()) {
+            post.comments.splice(commentIndex, 1);
+            await post.save();
+            res.json({ message: "Comment deleted" });
+        } else {
+            return res.status(400).json({ message: "You are not allowed to delete this comment" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const editCaption = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({ message: "Post not found" });
+
+        if (post.owner.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        post.caption = req.body.caption;
+        await post.save();
+        res.json({ message: "Caption changed" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { isAuth } from '../middleware/isAuth.js';
 import uploadFile from '../middleware/multer.js';
-import { createPost, deletePost } from '../controllers/postController.js';
+import { commentOnPost, createPost, deleteComment, deletePost, editCaption, getAllPosts, likeUnlikePost } from '../controllers/postController.js';
 
 const router = express.Router();
 
 router.post("/create", isAuth, uploadFile, createPost);
+router.put("/:id", isAuth, editCaption);
 router.delete("/:id", isAuth, deletePost);
+router.get("/all", isAuth, getAllPosts);
+router.post("/like/:id", isAuth, likeUnlikePost);
+router.post("/comment/:id", isAuth, commentOnPost);
+router.delete("/comment/:id", isAuth, deleteComment);
 
 export default router;
