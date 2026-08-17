b2b725f10a2bc9b94657db3be3b53acbb803b2d2
Nazim <165466730+MuhammedNazimAK@users.noreply.github.com>
2026-08-18 00:37:55 +0300

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -5,7 +5,7 @@ import { createContext } from "react";
 import io from "socket.io-client";
 import { UserData } from "./UserContext";
 
-const EndPoint = "http://localhost:7000";
+const EndPoint = "https://social-media-core.onrender.com";
 const SocketContext = createContext();
 
 export const SocketContextProvider = ({ children }) => {
